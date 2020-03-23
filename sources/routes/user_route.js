a986716d283d67fb72f051f147eb0b178746bae5
Muhammad Adam Fahmil 'Ilmi <adamfahmil020@gmail.com>
2020-03-23 13:39:35 +0700

    Remove cached

--- sources/routes/user_route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const {getAllUser, createUser} = require("../controllers/user_controller");
-
-router.route("/create").post(createUser);
-
-router.route("/get-all").get(getAllUser);
-
-module.exports = router;
